173225656171c937cfb59f605eb6d53166a7d85f
Diego R. Barbosa <diegokod@gmail.com>
2023-01-22 23:02:41 -0300

    Feat: Microserviço de avaliação de crédito criado

---
+++ msavalaiadorcredito/src/main/java/br/eidiego/msavalaiadorcredito/MsavalaiadorcreditoApplication.java
@@ -0,0 +1,13 @@
+package br.eidiego.msavalaiadorcredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsavalaiadorcreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsavalaiadorcreditoApplication.class, args);
+	}
+
+}
---
+++ msavalaiadorcredito/src/test/java/br/eidiego/msavalaiadorcredito/MsavalaiadorcreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.eidiego.msavalaiadorcredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsavalaiadorcreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
