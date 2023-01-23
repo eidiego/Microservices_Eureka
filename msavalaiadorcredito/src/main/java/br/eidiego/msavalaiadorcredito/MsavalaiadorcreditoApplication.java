ddd1564f70034f15b8d03c3aa111514e2fe9df5f
Diego R. Barbosa <diegokod@gmail.com>
2023-01-22 23:08:49 -0300

    Feat: msavaliadorcredito configurado no gateway

--- msavalaiadorcredito/src/main/java/br/eidiego/msavalaiadorcredito/MsavalaiadorcreditoApplication.java
+++ msavalaiadorcredito/src/main/java/br/eidiego/msavalaiadorcredito/MsavalaiadorcreditoApplication.java
@@ -2,8 +2,10 @@ package br.eidiego.msavalaiadorcredito;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsavalaiadorcreditoApplication {
 
 	public static void main(String[] args) {
