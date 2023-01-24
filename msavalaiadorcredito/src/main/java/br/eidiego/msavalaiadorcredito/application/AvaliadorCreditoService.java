d4e65d970880f17787c9c286839b49de197765de
Diego R. Barbosa <diegokod@gmail.com>
2023-01-23 22:44:21 -0300

    Feat: Exceptions for avaliador credito created

--- msavalaiadorcredito/src/main/java/br/eidiego/msavalaiadorcredito/application/AvaliadorCreditoService.java
+++ msavalaiadorcredito/src/main/java/br/eidiego/msavalaiadorcredito/application/AvaliadorCreditoService.java
@@ -1,26 +1,47 @@
 package br.eidiego.msavalaiadorcredito.application;
 
+import br.eidiego.msavalaiadorcredito.application.ex.DadosClienteNotFoundException;
+import br.eidiego.msavalaiadorcredito.application.ex.ErroComunicacaoMicroservicesException;
+import br.eidiego.msavalaiadorcredito.domain.model.CartaoCliente;
 import br.eidiego.msavalaiadorcredito.domain.model.DadosCliente;
 import br.eidiego.msavalaiadorcredito.domain.model.SituacaoCliente;
+import br.eidiego.msavalaiadorcredito.infra.clients.CartoesResourceClient;
 import br.eidiego.msavalaiadorcredito.infra.clients.ClienteResourceClient;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AvaliadorCreditoService {
 
     private final ClienteResourceClient clientesClient;
+    private final CartoesResourceClient cartoesClient;
 
-    public SituacaoCliente obterSituacaoCliente(String cpf) {
+    public SituacaoCliente obterSituacaoCliente(String cpf)
+            throws DadosClienteNotFoundException, ErroComunicacaoMicroservicesException {
 
-        ResponseEntity<DadosCliente> dadosClienteResponse = clientesClient.dadosCliente(cpf);
+        try{
+            ResponseEntity<DadosCliente> dadosClienteResponse = clientesClient.dadosCliente(cpf);
+            ResponseEntity<List<CartaoCliente>> cartoesResponse = cartoesClient.getCartoesByCliente(cpf);
 
-        return SituacaoCliente
-                .builder()
-                .cliente(dadosClienteResponse.getBody())
-                .build();
+            return SituacaoCliente
+                    .builder()
+                    .cliente(dadosClienteResponse.getBody())
+                    .cartoes(cartoesResponse.getBody())
+                    .build();
+
+        }catch (FeignException.FeignClientException e) {
+           int status = e.status();
+           if(HttpStatus.NOT_FOUND.value() == status) {
+               throw new DadosClienteNotFoundException();
+           }
+           throw new ErroComunicacaoMicroservicesException(e.getMessage(), status);
+      }
     }
 
 }
---
+++ msavalaiadorcredito/src/main/java/br/eidiego/msavalaiadorcredito/application/ex/DadosClienteNotFoundException.java
@@ -0,0 +1,8 @@
+package br.eidiego.msavalaiadorcredito.application.ex;
+
+public class DadosClienteNotFoundException extends Exception{
+
+    public DadosClienteNotFoundException() {
+        super("Dados do cliente não encontrados para os dados informados");
+    }
+}
